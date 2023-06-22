99a6c70bb179c2729bce82631b23d44d85ed683e
TheoThibodeau <teddythibodeau@gmail.com>
2023-06-22 18:30:17 -0400

    signup button and q and a basics

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,9 +6,14 @@ function Home ({setToken}) {
 
         const [username, setUsername] = useState('')
         const [password, setPassword] = useState('')
+        const [email, setEmail] = useState('')
 
         const handleUsername = (event) => {
             setUsername(event.target.value)
+        }
+
+        const handleEmail = (event) => {
+            setEmail(event.target.value)
         }
 
         const handleSubmit = (e) => {
@@ -20,8 +25,67 @@ function Home ({setToken}) {
                 })
                 .then((res) => setToken(res.data.auth_token))
         }
+
+        const handleRegister = (e) => {
+            e.preventDefault()
+            axios
+                .post('https://questionapi.onrender.com/auth/users/', {
+                    username: username,
+                    password: password,
+                    email: email,
+                })
+                .then((res) => setToken(res.data.auth_user))
+        }
     
+        // the handle register is not working right now,
+        // not exactly sure why, but i think it has something to do with the token
     return (
+    <> 
+              
+        <form onSubmit={handleRegister}>
+            <div>💻. New User Sign Up 💻</div>
+            <div>
+                <label>Email: </label>
+                <input 
+                type="text" 
+                name="name" 
+                id="name" 
+                value={email}
+                placeholder="Enter Your Email..."
+                onChange={handleEmail}
+                required></input>
+            </div>
+            <div>
+                <label>Username: </label>
+                <input 
+                type="text" 
+                name="name" 
+                id="name" 
+                // value={username}
+                placeholder="Choose a Username..."
+                value={username}
+                onChange={handleUsername}
+                required></input>
+            </div>
+            <div>
+                <label>Password: </label>
+                <input
+                type="text"
+                id="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Choose a Password..."
+                required></input>
+            </div>    
+            
+            <div>
+                <input 
+                type='submit' 
+                value="Register">
+                </input>
+            </div>
+        </form>
+        
         <form onSubmit={handleSubmit}>
             <div>
                 Login
@@ -53,64 +117,35 @@ function Home ({setToken}) {
                 </input>
             </div>
         </form>
+    </>
     )
 }
 
 export default Home
 
 
-// import { useState } from 'react'
-// import axios from 'axios'
+// import React, { useState } from 'react';
 
-// const Login = ({ setToken }) => {
-//   const [username, setUsername] = useState('')
-//   const [password, setPassword] = useState('')
+// function FormButton() {
+//   const [isFormVisible, setIsFormVisible] = useState(false);
 
-//   const handleUsernameChange = (event) => {
-//     console.log('username is changing')
-//     console.log(event)
-//     setUsername(event.target.value)
-//   }
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault()
-//     axios
-//       .post('https://drf-library-api-n3g8.onrender.com/auth/token/login/', {
-//         username: username,
-//         password: password,
-//       })
-//       .then((res) => setToken(res.data.auth_token))
-//   }
+//   const toggleFormVisibility = () => {
+//     setIsFormVisible(!isFormVisible);
+//   };
 
 //   return (
-//     <form className="m-5" onSubmit={handleSubmit}>
-//       <div>
-//         <label htmlFor="name">Username</label>
-//         <input
-//           type="text"
-//           name="name"
-//           id="name"
-//           value={username}
-//           onChange={handleUsernameChange}
-//           required
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="password">Password</label>
-//         <input
-//           type="password"
-//           name="password"
-//           id="password"
-//           value={password}
-//           onChange={(event) => setPassword(event.target.value)}
-//           required
-//         />
-//       </div>
-//       <div>
-//         <input type="submit" value="Log In" />
-//       </div>
-//     </form>
-//   )
+//     <div>
+//       <button onClick={toggleFormVisibility}>
+//         {isFormVisible ? 'Hide Form' : 'Show Form'}
+//       </button>
+
+//       {isFormVisible && (
+//         <form>
+//           {/* Add your form fields and submit button here */}
+//         </form>
+//       )}
+//     </div>
+//   );
 // }
 
-// export default Login
+// export default FormButton;
