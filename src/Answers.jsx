81f613a7989350464c499ca40443315a61233dba
superusermeaganrubino <meaganrubino@gmail.com>
2023-06-28 21:37:45 -0400

    delete-outtie-

--- src/Answers.jsx
+++ src/Answers.jsx
@@ -1,52 +1,46 @@
 import { useState, useEffect } from "react";
-import React from 'react'
-import axios from 'axios'
-import dayjs from 'dayjs'
+import React from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
 
-function Answers ({ token, questionId }) {
+function Answers({ token, questionId }) {
+  const [answers, setAnswers] = useState([]);
+  const [createAnswer, setCreateAnswer] = useState('');
 
-    const [ answers, setAnswers ] = useState([])
-    const [ createAnswer, setCreateAnswer ] = useState('')
+  useEffect(() => {
+    const headers = token ? { Authorization: `token ${token}` } : {};
 
+    axios
+      .get(`https://questionapi.onrender.com/questions/${questionId}/`, { headers })
+      .then((response) => setAnswers(response.data.answers))
+      .catch((error) => console.error(error));
+  }, [questionId, token]);
 
-    useEffect(() => {
-        axios
-        .get(`https://questionapi.onrender.com/questions/${questionId}/`)
-        .then((response) => setAnswers(response.data.answers))
-    }, [])
-// this get request makes it possible for an unauthenticated use to see answers to specific questions
-    useEffect(() => {
-        axios
-        .get(`https://questionapi.onrender.com/questions/${questionId}/`,
+  const handleCreateAnswer = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(
+        'https://questionapi.onrender.com/questions/answer/',
         {
-            headers: {
-                Authorization: `token ${token}`,
-            }
-        })
-        .then((response) => setAnswers(response.data.answers))
-    }, [])
-    
-    const handleCreateAnswer = (e) => {
-        e.preventDefault();
-        axios
-            .post('https://questionapi.onrender.com/questions/answer/', {
-            answer_text: createAnswer,
-            related_question: questionId,
-            }, {
-            headers: {
-                Authorization: `token ${token}`
-            }
-            })
-            .then(() => {
-            setCreateAnswer('');
-            })
-            // .catch((error) => console.error(error));
-        };
+          answer_text: createAnswer,
+          related_question: questionId,
+        },
+        {
+          headers: {
+            Authorization: `token ${token}`,
+          },
+        }
+      )
+      .then(() => {
+        setCreateAnswer('');
+      })
+      .catch((error) => console.error(error));
+  };
 
-    return (
-        
+  return (
     <>
-    <div className="answer-container">
+      <div className="answer-container">
         {answers.length > 0 ? (
           answers.map((answer) => (
             <div key={answer.id} className="answer-box">
@@ -58,17 +52,97 @@ function Answers ({ token, questionId }) {
             </div>
           ))
         ) : (
-            <p>Login to see answers...</p>
+          <p>Login to see answers...</p>
         )}
-            <input
-            type="text"
-            placeholder="Add your answer:"
-            onChange={(e) => setCreateAnswer(e.target.value)}>
-            </input>
-            <button onClick={handleCreateAnswer}>Add Answer</button>
-    </div>
+        <input
+          type="text"
+          placeholder="Add your answer:"
+          onChange={(e) => setCreateAnswer(e.target.value)}
+        />
+        <button onClick={handleCreateAnswer}>Add Answer</button>
+      </div>
     </>
-    )
+  );
 }
 
-export default Answers
+export default Answers;
+
+
+
+
+
+
+// import { useState, useEffect } from "react";
+// import React from 'react'
+// import axios from 'axios'
+// import dayjs from 'dayjs'
+
+// function Answers ({ token, questionId }) {
+
+//     const [ answers, setAnswers ] = useState([])
+//     const [ createAnswer, setCreateAnswer ] = useState('')
+
+
+//     useEffect(() => {
+//         axios
+//         .get(`https://questionapi.onrender.com/questions/${questionId}/`)
+//         .then((response) => setAnswers(response.data.answers))
+//     }, [])
+// // this get request makes it possible for an unauthenticated use to see answers to specific questions
+//     useEffect(() => {
+//         axios
+//         .get(`https://questionapi.onrender.com/questions/${questionId}/`,
+//         {
+//             headers: {
+//                 Authorization: `token ${token}`,
+//             }
+//         })
+//         .then((response) => setAnswers(response.data.answers))
+//     }, [])
+    
+//     const handleCreateAnswer = (e) => {
+//         e.preventDefault();
+//         axios
+//             .post('https://questionapi.onrender.com/questions/answer/', {
+//             answer_text: createAnswer,
+//             related_question: questionId,
+//             }, {
+//             headers: {
+//                 Authorization: `token ${token}`
+//             }
+//             })
+//             .then(() => {
+//             setCreateAnswer('');
+//             })
+//             // .catch((error) => console.error(error));
+//         };
+
+//     return (
+        
+//     <>
+//     <div className="answer-container">
+//         {answers.length > 0 ? (
+//             answers.map((answer) => (
+//             <div key={answer.id} className="answer-box">
+//                 <div className="answer-box-inner">
+//                 <p className="answer-author">{answer.answer_author}</p>
+//                 <p className="answer-text">{answer.answer_text}</p>
+//                 <p className="related-question">{dayjs(answer.answer_date).format('MM/DD/YYYY')}</p>
+//                 </div>
+//             </div>
+//             ))
+//         ) : (
+//             <p>Login to see answers...</p>
+//         )}
+//             <input
+//             type="text"
+//             placeholder="Add your answer:"
+//             onChange={(e) => setCreateAnswer(e.target.value)}>
+//             </input>
+//             <button onClick={handleCreateAnswer}>Add Answer</button>
+//     </div>
+//     </>
+//     )
+// }
+
+// export default Answers
--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,3 @@ export default App
     
     
     
-    
-    
-    
-    
-    
-    
-//   return (
-//     <>
-//       <Home setToken={setToken} />
-//       {token && (
-//         <>
-//           <Questions token={token} />
-//           <Answers token={token} />
-//         </>
