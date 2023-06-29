c3be3bbb9184e5f34cb46485f6c26d3b631b317f
superusermeaganrubino <meaganrubino@gmail.com>
2023-06-29 13:30:20 -0400

    profile-page-box

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,54 +3,60 @@ import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-function Profile( {token} ) {
+function Profile({ token }) {
 
-    const [userQuestions, setUserQuestions] = useState([])
-    const [userAnswers, setUserAnswers] = useState([])
+  const [userQuestions, setUserQuestions] = useState([])
+  const [userAnswers, setUserAnswers] = useState([])
+  const [isOpen, setIsOpen] = useState(true) // <== add this state
 
-    useEffect(() => {
-        axios
-        .get('https://questionapi.onrender.com/questions/user/',
+  // Get User Questions
+  useEffect(() => {
+    axios
+      .get('https://questionapi.onrender.com/questions/user/',
         {
-            headers: {
-                Authorization: `token ${token}`,
-            },
+          headers: {
+            Authorization: `token ${token}`,
+          },
         })
-        .then((response) => setUserQuestions(response.data))
-    }, [])
+      .then((response) => setUserQuestions(response.data))
+  }, [])
 
-    useEffect(() => {
-        axios
-        .get('https://questionapi.onrender.com/user/answers/',
+  // Get User Answers
+  useEffect(() => {
+    axios
+      .get('https://questionapi.onrender.com/user/answers/',
         {
-            headers: {
-                Authorization: `token ${token}`,
-            },
+          headers: {
+            Authorization: `token ${token}`,
+          },
         })
-        .then((response) => setUserAnswers(response.data))
-    }, [])
-    return (
-    <>
+      .then((response) => setUserAnswers(response.data))
+  }, [])
+
+  return (
+    isOpen && ( // <== change this to conditionally render the profile box
+      <div className={`profile-box ${isOpen ? "active" : ""}`}>
+        <button onClick={() => setIsOpen(false)}>Close</button> {/* Add this button to close the box */}
         <h1>Your Profile Page</h1>
         <h2>Questions</h2>
         <div>
-        {userQuestions.map((userQuestion) => (
+          {userQuestions.map((userQuestion) => (
             <ul>
-                <p>{userQuestion.question_text}</p>
+              <p>{userQuestion.question_text}</p>
             </ul>
-        ))}
+          ))}
         </div>
         <h2>Answers</h2>
         <div>
-        {userAnswers.map((userAnswer) => (
+          {userAnswers.map((userAnswer) => (
             <ul>
-                <p>{userAnswer.answer_text}</p>
+              <p>{userAnswer.answer_text}</p>
             </ul>
-        ))}
+          ))}
         </div>
-
-    </>       
+      </div>
     )
+  )
 }
 
 export default Profile
